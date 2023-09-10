93da702ecdae0b4d16ec1ee43a43377293db9f16
christopheruduekwe@gmail.com <christopheruduekwe@gmail.com>
2023-09-10 14:52:23 +0100

    Fix route error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import {
   Outlet,
-  createBrowserRouter,
-  RouterProvider,
+  Router,
   useParams,
+  Routes,
+  Route,
 } from 'react-router-dom';
+// createBrowserRouter,
+// RouterProvider,
 import ErrorPage from './routes/error-page';
 import Navbar from './components/Navbar/Navbar';
 import './index.css';
@@ -12,44 +15,63 @@ import Categories from './components/Categories/Categories';
 import MusicList from './components/MusicList/MusicList';
 
 function App() {
-  const GetMusiList = () => {
+  const GetMusicList = () => {
     const { id } = useParams();
     return <MusicList category={id} />;
   };
 
-  const View = () => (
-    <div className="App">
-      <Navbar />
-      <div className="container body">
-        <Outlet />
-      </div>
-      <footer>
-        <p>Copyright &copy; 2023 Chudisoft. All rights reserved.</p>
-      </footer>
-    </div>
-  );
+  // const View = () => (
+  //   <div className="App">
+  //     <Navbar />
+  //     <div className="container body">
+  //       <Outlet />
+  //     </div>
+  //     <footer>
+  //       <p>Copyright &copy; 2023 Chudisoft. All rights reserved.</p>
+  //     </footer>
+  //   </div>
+  // );
 
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <View />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: '/',
-          element: <Categories />,
-        },
-        {
-          path: '/Music/:id',
-          element: <GetMusiList />,
-          // element: <MusicList category="pop-music" />,
-        },
-      ],
-    },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: '/',
+  //     element: <View />,
+  //     errorElement: <ErrorPage />,
+  //     children: [
+  //       {
+  //         path: '/',
+  //         element: <Categories />,
+  //       },
+  //       {
+  //         path: '/Music/:id',
+  //         element: <GetMusiList />,
+  //         // element: <MusicList category="pop-music" />,
+  //       },
+  //     ],
+  //   },
+  // ]);
+
+  // return (
+  //   <RouterProvider router={router} />
+  // );
 
   return (
-    <RouterProvider router={router} />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container body">
+          <Routes>
+            <Route path="/" element={<Categories />} />
+            <Route path="/Music/:id" element={<GetMusicList />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+          <Outlet />
+        </div>
+        <footer>
+          <p>Copyright &copy; 2023 Chudisoft. All rights reserved.</p>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
